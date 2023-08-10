c9aaaaf0e4c9ca32efd1c63e7bb4c7cd37a4184f
kingtroga <trogaclassicman@gmail.com>
2023-08-10 19:11:43 +0100

    working on settings gui

--- py/widgets/chatitem.py
+++ py/widgets/chatitem.py
@@ -1,5 +1,12 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty
+from kivy.properties import (
+    StringProperty, 
+    NumericProperty,
+    BooleanProperty,
+    ListProperty,
+)
+from kivy.utils import get_color_from_hex
+    
 
 class ChatItem(BoxLayout):
     avatar = StringProperty()
@@ -7,3 +14,10 @@ class ChatItem(BoxLayout):
     lowertext = StringProperty()
     timeline = StringProperty()
     icon = StringProperty()
+    icon_size = NumericProperty()
+    is_bold = BooleanProperty()
+    text_icon_dis = NumericProperty()
+    uppertextY = NumericProperty()
+    lowertextY = NumericProperty()
+    back_color = ListProperty()
+    font_fam1 = StringProperty()
--- py/widgets/settings_nav.py
+++ py/widgets/settings_nav.py
@@ -1,4 +1,31 @@
 from kivymd.uix.bottomnavigation import MDBottomNavigationItem
+from kivy.clock import Clock
+from py.widgets.chatitem import ChatItem
+from kivy.properties import StringProperty, NumericProperty
+from kivy.metrics import dp
+from kivy.utils import get_color_from_hex
 
 class SettingsNav(MDBottomNavigationItem):
-    pass
+    def __init__(self, **kwargs):
+        super(SettingsNav, self).__init__(**kwargs)
+        Clock.schedule_once(self.show_settings, 0.5)
+
+    def show_settings(self, dt):
+        print("I'm a boy")
+        self.ids['aBox'].add_widget(
+            ChatItem(
+            avatar='images/4.jpg',
+            uppertext='Janet Doe',
+            lowertext='Who am I?',
+            timeline="",
+            icon="",
+            icon_size=dp(45),
+            is_bold=True,
+            text_icon_dis= 94,
+            uppertextY= 4,
+            lowertextY= 20,
+            pos= (self.ids['settingsLayout'].width - self.ids['settingsLayout'].width + 20 , self.ids['settingsLayout'].height -110),
+            back_color= get_color_from_hex("FFFFFF"),
+            font_fam1= "fonts/ARIALMTMEDIUM.TTF"
+            ))
+
