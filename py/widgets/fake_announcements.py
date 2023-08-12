20674bd5533466cf6e89347a9f3444d6c1400a66
kingtroga <trogaclassicman@gmail.com>
2023-08-12 17:10:43 +0100

    made the buttons on the create announcement page pushable

--- py/widgets/fake_announcements.py
+++ py/widgets/fake_announcements.py
@@ -3,7 +3,7 @@ from py.widgets.topbar import TopBar
 from py.widgets.smoothbutton import SmoothButton
 from kivy.lang import Builder
 from kivy.clock import Clock
-
+from kivy.utils import get_color_from_hex
 
 Builder.load_file("kv\widgets\\topbar.kv")
 Builder.load_file("kv\widgets\smoothbutton.kv")
@@ -36,4 +36,21 @@ class FakeAnnouncements(MDBottomNavigationItem):
         if self.backButton3.state == "down":
             self.backButton3.line_color = (205/255, 202/255, 202/255, 1)
         elif self.backButton3.state == "normal":
-            self.backButton3.line_color = (1, 1, 1, 1)   
+            self.backButton3.line_color = (1, 1, 1, 1)
+
+    def handlePublishBtnPress(self, btn):
+        btn.color = (121/255, 124/255, 123/255, 1)
+        btn.back_color = (36/255, 120/255, 109/255, 1)
+
+    def handlePublishBtnRelease(self, btn):
+        btn.color = 1, 1, 1, 1
+        btn.back_color = 32/255, 160/255, 144/255, 1
+
+
+    def handleCancelBtnPress(self, btn):
+        btn.color = get_color_from_hex("a23131")
+        btn.back_color = (250/255, 1, 255/255, 1)
+
+    def handleCancelBtnRelease(self, btn):
+        btn.color = 220/255, 0, 0, 1
+        btn.back_color = 241/255, 255/255, 253/255, 1 
