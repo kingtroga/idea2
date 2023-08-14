56b7093bee9607857aeca16e025421ba1016afa4
kingtroga <trogaclassicman@gmail.com>
2023-08-14 14:09:24 +0100

    named the screen

--- py/screens/message_screen.py
+++ py/screens/message_screen.py
@@ -3,3 +3,4 @@ from kivy.uix.screenmanager import Screen
 class MessageScreen(Screen):
     def __init__(self, **kwargs):
         super(MessageScreen, self).__init__(**kwargs)
+        self.name = "Screen10"
