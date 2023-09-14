3add2c9da1d64868718349e05a48e9bcc3f97e2c
kingtroga <trogaclassicman@gmail.com>
2023-09-14 12:01:31 +0100

    new functionality

--- py/widgets/messagebox.py
+++ py/widgets/messagebox.py
@@ -4,6 +4,10 @@ class MessageBox(TextInput):
     def __init__(self, **kwargs):
         super(MessageBox, self).__init__(**kwargs)
 
+   
+        
+
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
-        print(keycode)
+        if keycode[0] == 13:
+            self.text = ""
         return super().keyboard_on_key_down(window, keycode, text, modifiers)
