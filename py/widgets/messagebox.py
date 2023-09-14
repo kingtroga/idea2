c39fee3ff854436e9d390b679557260482f3e550
kingtroga <trogaclassicman@gmail.com>
2023-09-14 12:02:02 +0100

    cleaning

--- py/widgets/messagebox.py
+++ py/widgets/messagebox.py
@@ -4,9 +4,6 @@ class MessageBox(TextInput):
     def __init__(self, **kwargs):
         super(MessageBox, self).__init__(**kwargs)
 
-   
-        
-
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
         if keycode[0] == 13:
             self.text = ""
